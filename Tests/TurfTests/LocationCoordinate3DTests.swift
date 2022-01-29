a76a934821c6d75f7ab38e2d132436feb511868a
Paul Finlay <paul@hiiker.co>
2022-01-29 11:11:52 +0000

    asdasd

--- Tests/TurfTests/LocationCoordinate3DTests.swift
+++ Tests/TurfTests/LocationCoordinate3DTests.swift
@@ -7,9 +7,15 @@ import Turf
 class LocationCoordinate3DTests: XCTestCase {
         
     func testCreateCoordinate() {
-        let data = try! Fixture.geojsonData(from: "featurecollection-3d")!
-        let abc3d = LocationCoordinate3DCodable.init(from: coords)
+        let data2 = try! Fixture.geojsonData(from: "featurecollection-3d")
+        let geojson = try! JSONDecoder().decode(GeoJSONObject.self, from: data)
+        guard case let .featureCollection(featureCollection) = geojson else { return XCTFail() }
+        let feature = featureCollection.features.first
         
+        guard case let .lineString(lineString) = lineStringFeature.geometry else {
+            XCTFail()
+            return
+        }
     }
 }
 
